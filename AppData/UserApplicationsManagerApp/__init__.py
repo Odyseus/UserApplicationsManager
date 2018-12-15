c774de9617919a847db71d0654770f6e9368031f
Odyseus <venusyadonis@gmail.com>
2018-12-15 07:28:06 -0300

    __init__.py
    - Homogenization of the __appdescription__ variable.

--- AppData/UserApplicationsManagerApp/__init__.py
+++ AppData/UserApplicationsManagerApp/__init__.py
@@ -3,5 +3,5 @@
 
 __status__ = "Unstable"
 __appname__ = "User Applications Manager"
-__appdescription__ = "Utility to manage applications inside a user's home directory."
+__appdescription__ = "CLI utility to manage applications inside a user's home directory."
 __version__ = "0.0.0"
