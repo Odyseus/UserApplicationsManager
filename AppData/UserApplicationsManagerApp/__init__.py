e782f071b180e1433fecf76d068e409938a4368d
Odyseus <venusyadonis@gmail.com>
2020-03-08 09:13:44 -0300

    Updated manual page and bump version

--- AppData/UserApplicationsManagerApp/__init__.py
+++ AppData/UserApplicationsManagerApp/__init__.py
@@ -3,7 +3,7 @@
 __status__ = "Unstable"
 __appname__ = "User Applications Manager"
 __appdescription__ = "CLI utility to manage applications inside a user's home directory."
-__version__ = "2020-02-19 12:00:13.467"
+__version__ = "2020-03-08 09:10:52.487"
 
 if __name__ == "__main__":
     pass
